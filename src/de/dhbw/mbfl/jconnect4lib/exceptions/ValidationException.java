3cc9ab4d4ef175f23f454096a2dcbac1c2503385
Florian Loch <florian.loch@gmail.com>
2014-10-23 15:25:14 +0200

    Enhance ValidationException

--- src/de/dhbw/mbfl/jconnect4lib/exceptions/ValidationException.java
+++ src/de/dhbw/mbfl/jconnect4lib/exceptions/ValidationException.java
@@ -1,15 +1,21 @@
 package de.dhbw.mbfl.jconnect4lib.exceptions;
 
+import de.dhbw.mbfl.jconnect4lib.board.Position;
+
 /**
  *
  * @author Maurice Busch & Florian Loch
  */
-public class ValidationException extends Exception
-{
-
-    public ValidationException(String msg)
-    {
+public class ValidationException extends Exception {
+    private Position position;
+     
+    public ValidationException(String msg, Position position) {
         super(msg);
+        this.position = position;
     }
 
+    @Override
+    public String getMessage() {
+        return super.getMessage() + " Position: " + position;
+    }
 }
