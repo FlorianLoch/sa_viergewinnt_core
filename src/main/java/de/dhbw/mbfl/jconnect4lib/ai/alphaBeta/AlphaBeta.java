1830bcd1ec9c5aaf70d3d367dd061baca9d82905
Florian Loch <florian.loch@gmail.com>
2015-02-02 17:22:46 +0100

    Adjust levels for alphaBeta calc test

--- src/main/java/de/dhbw/mbfl/jconnect4lib/ai/alphaBeta/AlphaBeta.java
+++ src/main/java/de/dhbw/mbfl/jconnect4lib/ai/alphaBeta/AlphaBeta.java
@@ -30,7 +30,7 @@ public class AlphaBeta {
         logFile = new FileWriter("alphaBeta.log");
         
         int[] levelsToCheck;
-        levelsToCheck = new int[]{7, 8, 9, 30, 42, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        levelsToCheck = new int[]{7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 42, 30};
         
         for (int levelToCheck : levelsToCheck) {
             Board currentBoard = new Board();
